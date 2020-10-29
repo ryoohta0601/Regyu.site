0251654924adc67afe27769ee4071ed511645127
ryoohta0601 <s10.i8.h88@outlook.jp>
2020-10-29 17:11:20 +0900

    rank

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,11 @@
 class ToppagesController < ApplicationController
   def index
+    @restaurants = Restaurant.all.each do |restaurant|
+      restaurant.average = restaurant.average_rate
+    end
+
+    @restaurants = @restaurants.sort_by { |restaurant| [restaurant.average ? 1 : 0, restaurant.average] }.reverse
+
     @reviews = Review.all.order(created_at: :desc).limit(6)
   end
 end
