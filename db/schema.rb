54963f1815ad7a0a1bb4ce97bbb4e41f5a777e39
ryoohta0601 <s10.i8.h88@outlook.jp>
2020-10-31 04:17:42 +0900

    calender

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_153006) do
+ActiveRecord::Schema.define(version: 2020_10_30_075238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2020_10_29_153006) do
     t.index ["restaurant_id", "user_id"], name: "index_bookmarks_on_restaurant_id_and_user_id", unique: true
     t.index ["restaurant_id"], name: "index_bookmarks_on_restaurant_id"
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "start_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "likes", force: :cascade do |t|
