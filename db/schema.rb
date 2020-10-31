3d0fa2561cce86656bfa80fc8fe130b7bfb8fffe
ryoohta0601 <s10.i8.h88@outlook.jp>
2020-11-01 04:21:25 +0900

    browsing-history

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_31_072022) do
+ActiveRecord::Schema.define(version: 2020_10_31_183304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2020_10_31_072022) do
     t.index ["restaurant_id", "user_id"], name: "index_bookmarks_on_restaurant_id_and_user_id", unique: true
     t.index ["restaurant_id"], name: "index_bookmarks_on_restaurant_id"
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
+
+  create_table "browsing_histories", force: :cascade do |t|
+    t.bigint "restaurant_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_browsing_histories_on_restaurant_id"
+    t.index ["user_id"], name: "index_browsing_histories_on_user_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -89,6 +98,8 @@ ActiveRecord::Schema.define(version: 2020_10_31_072022) do
 
   add_foreign_key "bookmarks", "restaurants"
   add_foreign_key "bookmarks", "users"
+  add_foreign_key "browsing_histories", "restaurants"
+  add_foreign_key "browsing_histories", "users"
   add_foreign_key "events", "users"
   add_foreign_key "likes", "reviews"
   add_foreign_key "likes", "users"
